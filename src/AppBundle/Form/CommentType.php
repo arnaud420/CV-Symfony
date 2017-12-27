35bea680637ee31467dfb3097b429f204ea451e0
arnaud420 <arnaud_lafon@hotmail.fr>
2017-12-27 12:39:13 +0100

    update post view

--- src/AppBundle/Form/CommentType.php
+++ src/AppBundle/Form/CommentType.php
@@ -2,11 +2,8 @@
 
 namespace AppBundle\Form;
 
-use Sonata\AdminBundle\Form\Type\Filter\NumberType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
